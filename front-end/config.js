2f2e910486bfe6c189250eb229ff7d7a7359fc2c
lukinhasc-dev <lucas.sca2022@gmail.com>
2026-04-15 11:00:42 -0300

    fix: ajustes no config.js, inserindo url correta após deploy do back-end

--- front-end/config.js
+++ front-end/config.js
@@ -1,5 +1,5 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-export const API_URL = isLocal ? "http://localhost:3000" : "https://api-mej.onrender.com"; //Alterar para o URL do Render quando for colocar em produção.
+export const API_URL = isLocal ? "http://localhost:3000" : "https://platataforma-mej.onrender.com";
 
 export const SUPABASE_URL = "https://heamrvsbpsyfsybnwofj.supabase.co"
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlYW1ydnNicHN5ZnN5Ym53b2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODg0NjEsImV4cCI6MjA4ODc2NDQ2MX0.5cnqLWMWW1XUs55cBJ5gRy_1DcWL7lCSRR3NsWz3eWo"
