5818befb16a989d80eb5d0b86cf380699fbb50ac
lukinhasc-dev <lucas.sca2022@gmail.com>
2026-04-15 16:28:51 -0300

    fix(env): ajustes no env da api key do resent

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -12,6 +12,9 @@ if (!SUPABASE_URL || !SUPABASE_KEY) {
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 export default pool
